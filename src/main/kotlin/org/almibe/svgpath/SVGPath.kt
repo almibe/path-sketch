46fa4ceb4daee4d94f720cdf779e66afc627fbdf
Alex Berry <berry3@indiana.edu>
2018-08-20 01:55:42 -0400

    replacing startPos with PathDimensions

--- src/main/kotlin/org/almibe/svgpath/SVGPath.kt
+++ src/main/kotlin/org/almibe/svgpath/SVGPath.kt
@@ -17,12 +17,12 @@ class SVGPath(val commands: List<SVGCommand>) {
         }
         while (itr.hasNext()) {
             val next = itr.next()
-            val startPos = next.startPos()
-            if (startPos.first < x) {
-                x = startPos.first
+            val pathDimensions = next.pathDimensions()
+            if (pathDimensions.x0 < x) {
+                x = pathDimensions.x0
             }
-            if (startPos.second < y) {
-                y = startPos.second
+            if (pathDimensions.y0 < y) {
+                y = pathDimensions.y0
             }
         }
 
@@ -44,8 +44,10 @@ class SVGPath(val commands: List<SVGCommand>) {
     }
 }
 
+data class PathDimensions(val x0: Double, val y0: Double, val x1: Double, val y1: Double)
+
 interface SVGCommand {
-    fun startPos(): Pair<Double, Double>
+    fun pathDimensions(): PathDimensions
     fun scale(scaleBy: Double): SVGCommand
     fun toPathString(): String
 }
@@ -53,172 +55,121 @@ interface SVGCommand {
 data class Move(val x: Double, val y: Double): SVGCommand {
     override fun toPathString(): String = "M $x $y"
     override fun scale(scaleBy: Double): SVGCommand = Move(x * scaleBy, y * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return x to y
-    }
+    override fun pathDimensions() = PathDimensions(x, y, x, y)
 }
 
 data class RelativeMove(val x: Double, val y: Double): SVGCommand {
     override fun toPathString(): String = "m $x $y"
     override fun scale(scaleBy: Double): SVGCommand = RelativeMove(x * scaleBy, y * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return x to y
-    }
+    override fun pathDimensions() = PathDimensions(x, y, x, y)
 }
 
 data class Line(val x: Double, val y: Double): SVGCommand {
     override fun toPathString(): String = "L $x $y"
     override fun scale(scaleBy: Double): SVGCommand = Line(x * scaleBy, y * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return x to y
-    }
+    override fun pathDimensions() = PathDimensions(x, y, x, y)
 }
 
 data class RelativeLine(val x: Double, val y: Double): SVGCommand {
     override fun toPathString(): String = "l $x $y"
     override fun scale(scaleBy: Double): SVGCommand = RelativeLine(x * scaleBy, y * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return x to y
-    }
+    override fun pathDimensions() = PathDimensions(x, y, x, y)
 }
 
 data class HorizontalLine(val x: Double): SVGCommand {
     override fun toPathString(): String = "H $x"
     override fun scale(scaleBy: Double): SVGCommand = HorizontalLine(x * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return x to Double.MAX_VALUE
-    }
+    override fun pathDimensions() = PathDimensions(x, Double.MAX_VALUE, x, Double.MIN_VALUE)
 }
 
 data class RelativeHorizontalLine(val x: Double): SVGCommand {
     override fun toPathString(): String = "h $x"
     override fun scale(scaleBy: Double): SVGCommand = RelativeHorizontalLine(x * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return x to Double.MAX_VALUE
-    }
+    override fun pathDimensions() = PathDimensions(x, Double.MAX_VALUE, x, Double.MIN_VALUE)
 }
 
 data class VerticleLine(val y: Double): SVGCommand {
     override fun toPathString(): String = "V $y"
     override fun scale(scaleBy: Double): SVGCommand = VerticleLine(y * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return Double.MAX_VALUE to y
-    }
+    override fun pathDimensions() = PathDimensions(Double.MAX_VALUE, y, Double.MIN_VALUE, y)
 }
 
 data class RelativeVerticleLine(val y: Double): SVGCommand {
     override fun toPathString(): String = "v $y"
     override fun scale(scaleBy: Double): SVGCommand = RelativeVerticleLine(y * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return Double.MAX_VALUE to y
-    }
+    override fun pathDimensions() = PathDimensions(Double.MAX_VALUE, y, Double.MIN_VALUE, y)
 }
 
 class ClosePath: SVGCommand {
     override fun toPathString(): String = "z"
     override fun scale(scaleBy: Double): SVGCommand = ClosePath()
-
-    override fun startPos(): Pair<Double, Double> {
-        return Double.MAX_VALUE to Double.MAX_VALUE
-    }
+    override fun pathDimensions() = PathDimensions(Double.MAX_VALUE, Double.MAX_VALUE, Double.MIN_VALUE, Double.MIN_VALUE)
 }
 
 data class CubicBezierCurve(val x0: Double, val y0: Double, val x1: Double, val y1: Double, val x2: Double, val y2: Double): SVGCommand {
     override fun toPathString(): String = "C $x0 $y0 $x1 $y1 $x2 $y2"
     override fun scale(scaleBy: Double): SVGCommand = CubicBezierCurve(x0 * scaleBy, y0 * scaleBy, x1 * scaleBy, y1 * scaleBy, x2 * scaleBy, y2 *scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return listOf(x0, x1, x2).min()!! to listOf(y0, y1, y2).min()!!
-    }
+    override fun pathDimensions() = PathDimensions(listOf(x0, x1, x2).min()!!,listOf(y0, y1, y2).min()!!,
+                listOf(x0, x1, x2).max()!!,listOf(y0, y1, y2).max()!!)
 }
 
 data class RelativeCubicBezierCurve(val x0: Double, val y0: Double, val x1: Double, val y1: Double, val x2: Double, val y2: Double): SVGCommand {
     override fun toPathString(): String = "c $x0 $y0 $x1 $y1 $x2 $y2"
     override fun scale(scaleBy: Double): SVGCommand = RelativeCubicBezierCurve(x0 * scaleBy, y0 * scaleBy, x1 * scaleBy, y1 * scaleBy, x2 * scaleBy, y2 *scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return listOf(x0, x1, x2).min()!! to listOf(y0, y1, y2).min()!!
-    }
+    override fun pathDimensions() = PathDimensions(listOf(x0, x1, x2).min()!!,listOf(y0, y1, y2).min()!!,
+            listOf(x0, x1, x2).max()!!,listOf(y0, y1, y2).max()!!)
 }
 
 data class SmoothCubicBezierCurve(val x0: Double, val y0: Double, val x1: Double, val y1: Double): SVGCommand {
     override fun toPathString(): String = "S $x0 $y0 $x1 $y1"
     override fun scale(scaleBy: Double): SVGCommand = SmoothCubicBezierCurve(x0 * scaleBy, y0 * scaleBy, x1 * scaleBy, y1 * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return listOf(x0, x1).min()!! to listOf(y0, y1).min()!!
-    }
+    override fun pathDimensions() = PathDimensions(listOf(x0, x1).min()!!,listOf(y0, y1).min()!!,
+            listOf(x0, x1).max()!!,listOf(y0, y1).max()!!)
 }
 
 data class SmoothRelativeCubicBezierCurve(val x0: Double, val y0: Double, val x1: Double, val y1: Double): SVGCommand {
     override fun toPathString(): String = "s $x0 $y0 $x1 $y1"
     override fun scale(scaleBy: Double): SVGCommand = SmoothRelativeCubicBezierCurve(x0 * scaleBy, y0 * scaleBy, x1 * scaleBy, y1 * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return listOf(x0, x1).min()!! to listOf(y0, y1).min()!!
-    }
+    override fun pathDimensions() = PathDimensions(listOf(x0, x1).min()!!,listOf(y0, y1).min()!!,
+            listOf(x0, x1).max()!!,listOf(y0, y1).max()!!)
 }
 
 data class QuadraticeBezierCurve(val x0: Double, val y0: Double, val x1: Double, val y1: Double): SVGCommand {
     override fun toPathString(): String = "Q $x0 $y0 $x1 $y1"
     override fun scale(scaleBy: Double): SVGCommand = QuadraticeBezierCurve(x0 * scaleBy, y0 * scaleBy, x1 * scaleBy, y1 * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return listOf(x0, x1).min()!! to listOf(y0, y1).min()!!
-    }
+    override fun pathDimensions() = PathDimensions(listOf(x0, x1).min()!!,listOf(y0, y1).min()!!,
+            listOf(x0, x1).max()!!,listOf(y0, y1).max()!!)
 }
 
 data class RelativeQuadraticeBezierCurve(val x0: Double, val y0: Double, val x1: Double, val y1: Double): SVGCommand {
     override fun toPathString(): String = "q $x0 $y0 $x1 $y1"
     override fun scale(scaleBy: Double): SVGCommand = RelativeQuadraticeBezierCurve(x0 * scaleBy, y0 * scaleBy, x1 * scaleBy, y1 * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return listOf(x0, x1).min()!! to listOf(y0, y1).min()!!
-    }
+    override fun pathDimensions() = PathDimensions(listOf(x0, x1).min()!!,listOf(y0, y1).min()!!,
+            listOf(x0, x1).max()!!,listOf(y0, y1).max()!!)
 }
 
 data class SmoothQuadraticeBezierCurve(val x: Double, val y: Double): SVGCommand {
     override fun toPathString(): String = "T $x $y"
     override fun scale(scaleBy: Double): SVGCommand = SmoothQuadraticeBezierCurve(x * scaleBy, y * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return x to y
-    }
+    override fun pathDimensions() = PathDimensions(x, y, x, y)
 }
 
 data class SmoothRelativeQuadraticeBezierCurve(val x: Double, val y: Double): SVGCommand {
     override fun toPathString(): String = "t $x $y"
     override fun scale(scaleBy: Double): SVGCommand = SmoothRelativeQuadraticeBezierCurve(x * scaleBy, y * scaleBy)
-
-    override fun startPos(): Pair<Double, Double> {
-        return x to y
-    }
+    override fun pathDimensions() = PathDimensions(x, y, x, y)
 }
 
 data class Arc(val rx: Double, val ry: Double, val xRotation: Double, val largeArcFlag: Double,
                val sweepFlag: Double, val x: Double, val y: Double): SVGCommand {
     override fun toPathString(): String = TODO()
     override fun scale(scaleBy: Double): SVGCommand = TODO()
-
-    override fun startPos(): Pair<Double, Double> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun pathDimensions() = TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 }
 
 data class RelativeArc(val rx: Double, val ry: Double, val xRotation: Double, val largeArcFlag: Double,
                        val sweepFlag: Double, val x: Double, val y: Double): SVGCommand {
     override fun toPathString(): String = TODO()
     override fun scale(scaleBy: Double): SVGCommand = TODO()
-
-    override fun startPos(): Pair<Double, Double> {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun pathDimensions() = TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
 }
